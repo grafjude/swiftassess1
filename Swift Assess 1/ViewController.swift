c47d7fbf185d572685a5028c1a7826d13b81a4b2
JG <grafjude@badinhs.org>
2016-09-19 10:53:05 -0400

    First Commit

---
+++ Swift Assess 1/ViewController.swift
@@ -0,0 +1,28 @@
+/*
+  ViewController.swift
+  Swift Assess 1
+
+  Created by Jude Graf on 9/19/16.
+  Copyright © 2016 Jude Graf. All rights reserved.
+*/
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var myLabel2: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor=UIColor.yellow
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
